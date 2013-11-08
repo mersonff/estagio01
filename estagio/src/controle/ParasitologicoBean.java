aef3f2118407d6cdf04f183feaa05f49c11f6afd
emerson <emerson@192.168.1.7>
2013-11-08 20:29:05 -0300

    Parasitológico de Fezes: Concluído.

--- estagio/src/controle/ParasitologicoBean.java
+++ estagio/src/controle/ParasitologicoBean.java
@@ -1,6 +1,7 @@
 package controle;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -16,6 +17,19 @@ import dao.ParasitologicoJPADAO;
 public class ParasitologicoBean extends AbstractBean {
 	private Parasitologico parasitologico;
 	private List<Parasitologico> parasitologicos;
+	private List<Parasitologico> parasitologicoEmEspera;
+	private List<Parasitologico> parasitologicoEmAberto;
+	private List<Parasitologico> filteredParasitologicos;
+	private String[] metodos = { "Baerman", "Simples", "Hamatoxilina",
+			"Seriado" };
+	private String[] ovos = { "Ascaris Lumbricóides", "Ancilostomideo",
+			"Trichuris Trichiura", "Enterobius Vermiculares",
+			"Strongiloides Larvas", "Taenia SP", "Schistosoma Mansonu",
+			"Hymenolepis Nana", "Hymenolepis Diminuta" };
+	private String[] protozoarios = { "Entamoeba Histolitica",
+			"Entamoeba Coli", "Iodamoeba Butchili", "Endolimax Nana",
+			"Trichomonas Hominis", "Chilomati Mesnili", "Giarda Lamblia",
+			"Balantio II Coli", "Isopora Beli" };
 
 	public ParasitologicoBean() {
 		this.setParasitologico(new Parasitologico());
@@ -23,16 +37,44 @@ public class ParasitologicoBean extends AbstractBean {
 		pesquisarTodos();
 	}
 
-	public void cadastrar() {
+	public void agendar() {
 		ParasitologicoDAO operDAO = new ParasitologicoJPADAO();
 		PacienteDAO pDAO = new PacienteJPADAO();
-		Paciente p = pDAO.find(this.parasitologico.getPaciente().getNumeroSus());
+		Paciente p = pDAO
+				.find(this.parasitologico.getPaciente().getNumeroSus());
 		if (p != null) {
+			this.parasitologico.setStatus("Em espera");
 			operDAO.save(this.parasitologico);
 			displayInfoMessageToUser("Cadastrado com sucesso!");
+			this.parasitologico = new Parasitologico();
 		} else {
-			displayInfoMessageToUser("Paciente não cadastrado." + "");
+			displayErrorMessageToUser("Paciente não cadastrado: Por favor, cadastre o paciente e tente novamente.");
 		}
+
+	}
+
+	public void remarcar() {
+		ParasitologicoDAO operDAO = new ParasitologicoJPADAO();
+		this.parasitologico.setStatus("Em espera");
+		operDAO.save(this.parasitologico);
+		displayInfoMessageToUser("Cadastrado com sucesso!");
+	}
+
+	public void cadastrar() {
+		ParasitologicoDAO operDAO = new ParasitologicoJPADAO();
+		this.parasitologico.setDataPedido(new Date());
+		this.parasitologico.setStatus("Em aberto");
+		operDAO.save(this.parasitologico);
+		displayInfoMessageToUser("Cadastrado com sucesso!");
+	}
+
+	public void atualizar() {
+		ParasitologicoDAO operDAO = new ParasitologicoJPADAO();
+		this.parasitologico.setStatus("Concluído");
+		this.parasitologico.setDataEntrega(new Date());
+		operDAO.save(this.parasitologico);
+		displayInfoMessageToUser("Cadastrado com sucesso!");
+
 	}
 
 	public void pesquisarTodos() {
@@ -40,10 +82,23 @@ public class ParasitologicoBean extends AbstractBean {
 		this.parasitologicos = operDAO.find();
 	}
 
-	public void excluir(Parasitologico parasitologico) {
+	public void excluir() {
 		ParasitologicoDAO operDAO = new ParasitologicoJPADAO();
-		operDAO.delete(parasitologico);
+		operDAO.delete(this.parasitologico);
 		displayInfoMessageToUser("Excluido com sucesso!");
+		this.parasitologicos = operDAO.find();
+
+	}
+
+	public void find() {
+		PacienteDAO pDAO = new PacienteJPADAO();
+		Paciente p = pDAO
+				.find(this.parasitologico.getPaciente().getNumeroSus());
+		if (p != null) {
+			displayInfoMessageToUser("Paciente encontrado.");
+		} else {
+			displayErrorMessageToUser("Paciente não encontrado. Por favor, realize o cadastro do paciente.");
+		}
 	}
 
 	public Parasitologico getParasitologico() {
@@ -60,6 +115,69 @@ public class ParasitologicoBean extends AbstractBean {
 
 	public void setParasitologicos(List<Parasitologico> parasitologicos) {
 		this.parasitologicos = parasitologicos;
+	}
+
+	public List<Parasitologico> getFilteredParasitologicos() {
+		return filteredParasitologicos;
+	}
+
+	public void setFilteredParasitologicos(
+			List<Parasitologico> filteredParasitologicos) {
+		this.filteredParasitologicos = filteredParasitologicos;
+	}
+
+	public List<Parasitologico> getParasitologicoEmEspera() {
+		List<Parasitologico> temp = new ArrayList<Parasitologico>();
+		for (Parasitologico b : parasitologicos) {
+			if (b.getStatus().equals("Em espera"))
+				temp.add(b);
+		}
+		parasitologicoEmEspera = temp;
+		return parasitologicoEmEspera;
+	}
+
+	public void setParasitologicoEmEspera(
+			List<Parasitologico> parasitologicoEmEspera) {
+		this.parasitologicoEmEspera = parasitologicoEmEspera;
+	}
+
+	public List<Parasitologico> getParasitologicoEmAberto() {
+		List<Parasitologico> temp = new ArrayList<Parasitologico>();
+		for (Parasitologico b : parasitologicos) {
+			if (b.getStatus().equals("Em aberto"))
+				temp.add(b);
+		}
+		parasitologicoEmAberto = temp;
+		return parasitologicoEmAberto;
+	}
+
+	public void setParasitologicoEmAberto(
+			List<Parasitologico> parasitologicoEmAberto) {
+		this.parasitologicoEmAberto = parasitologicoEmAberto;
+	}
+
+	public String[] getMetodos() {
+		return metodos;
+	}
+
+	public void setMetodos(String[] metodos) {
+		this.metodos = metodos;
+	}
+
+	public String[] getOvos() {
+		return ovos;
+	}
+
+	public void setOvos(String[] ovos) {
+		this.ovos = ovos;
+	}
+
+	public String[] getProtozoarios() {
+		return protozoarios;
+	}
+
+	public void setProtozoarios(String[] protozoarios) {
+		this.protozoarios = protozoarios;
 	}
 
 }
